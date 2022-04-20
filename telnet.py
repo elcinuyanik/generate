c58b37b28ed7df6ac8d75aa21604bbdcd7afa7c8
elcinuyanik <100758010+elcinuyanik@users.noreply.github.com>
2022-04-20 10:41:51 +0300

    Delete telnet.py

--- telnet.py
+++
@@ -1,4 +0,0 @@
-from SimpleTelnetMail import TelnetMail
-
-client = TelnetMail("192.168.10.225", port=25, from_ = "elcin@dbx.local", to = ["elcin@dfxtest.local"], message = "Secret and secure email with Telnet.")
-client.send_mail()
